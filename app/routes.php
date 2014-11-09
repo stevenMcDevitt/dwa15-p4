9b1639fb1fc1ac32b2dc76cce440521083af2526
Steven McDevitt <smcdevitt@g.harvard.edu>
2014-11-09 16:00:22 -0500

    Initial Commit

---
+++ app/routes.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello');
+});
+
+Route::get('dbtest', function() {
+
+    # Print environment
+
+    echo 'Project 4'.'<br><br>';
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
+
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+# DWA15 - Project 4 - TBD
+
+## Live URL
+<http://p4.iitbotftbd.com>
+
+## Description
+It is the business of the future to be dangerous.
+
+## Demo
+A demo and presentation will be provided in the in-person section.
+
+## Details for teaching team
+It is the business of the future to be dangerous.
+
+## Outside code
+* HTML5 Boilerplate: http://html5boilerplate.com/
+* Bootstrap: http://getbootstrap.com/
